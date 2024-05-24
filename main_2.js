835b9e0ec995cf5b6bf305262eba74455d23796c
evinanil2 <123241343+evinanil2@users.noreply.github.com>
2024-05-24 14:11:42 +0400

    Add files via upload

--- main_2.js
+++ main_2.js
@@ -14,7 +14,22 @@ function setup() {
 }
 function draw() {
     image(img, 0, 0, 640, 420);
-   }
+    if(status !="")
+        {
+            for(i = 0; i < objects.length; i++)
+                {
+                   document.getElementById("status").innerHTML = "Status : Object Detected";
+
+                   fill("#FF0000")
+                   percent = floor(objects[i].confidence*100);
+                   text(objects[i].label+ " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+                   noFill();
+                   stroke("#FF0000");
+                   rect(objects[i].x,objects[i].y, objects[i].width, objects[i].height);
+                }
+        }
+    
+}
 function modelLoaded(){
     console.log("model Loaded!")
     status = true;
@@ -26,8 +41,8 @@ function modelLoaded(){
             console.log(error);
         }
         console.log(results);
+        objects = results;
     }
-
 function back()
 {
     window.location = "index.html";
